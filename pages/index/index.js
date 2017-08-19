faaa2a9cdea5234f1774e46e3b6dddcd9718ba96
[banpai] <[banpai@yeah.net]>
2017-08-19 17:55:58 +0800

    添加电话功能和地图功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -12,10 +12,30 @@ Page({
       url: '../logs/logs'
     })
   },
+  //查看地图
+  seeMap: function(){
+    wx.getLocation({
+      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
+      success: function(res) {
+        var latitude = res.latitude
+        var longitude = res.longitude
+        wx.openLocation({
+          latitude: latitude,
+          longitude: longitude,
+          scale: 28
+        })
+      }
+    })
+  },
+  //打电话
+  tapCall: function(){
+    wx.makePhoneCall({
+      phoneNumber: '15698745632' //仅为示例，并非真实的电话号码
+    })
+  },
   onLoad: function () {
     console.log('onLoad')
     var that = this
-    
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
