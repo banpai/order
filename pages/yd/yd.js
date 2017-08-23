c74952e822986042c0de8f84da16246bac122870
banpai <banpai@yeah.net>
2017-08-23 17:17:26 +0800

    添加预订选择时间页面

--- pages/yd/yd.js
+++ pages/yd/yd.js
@@ -4,24 +4,37 @@ var app = getApp()
 Page({
   data: {
     motto: 'Hello World',
-    userInfo: {}
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+    userInfo: {},
+    time: "2017-08-26"
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
-    
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
-    })
+  },
+  nextbp: function () {
+
+  },
+  open: function () {
+    var that = this;
+    var tarray = [
+      "2017-08-26",
+      "2017-08-27",
+      "2017-08-28",
+      "2017-08-29",
+      "2017-08-30",
+      "2017-08-31"
+    ];
+    wx.showActionSheet({
+      itemList: tarray,
+      success: function (res) {
+        console.log(JSON.stringify(res));
+        if (!res.cancel) {
+          console.log(res.tapIndex)
+          that.setData({
+            time: tarray[res.tapIndex]
+          });
+        }
+      }
+    });
   }
 })
